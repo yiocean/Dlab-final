757b0304e0ac0bb48c1eee53ae1547d23469fe89
Amber-coding <amber.555yi@gmail.com>
2024-12-06 20:08:10 +0800

    add vivado

---
+++ final_snake/lab10_for_student/Lab10/lab10/lab10.srcs/sources_1/new/start.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/12/06 20:01:46
+// Design Name: 
+// Module Name: start
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module start(
+
+    );
+endmodule
