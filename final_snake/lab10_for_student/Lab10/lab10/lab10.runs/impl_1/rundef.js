3c316bdd068535339503242ac9a5b78b0ff6e495
Amber-coding <amber.555yi@gmail.com>
2024-12-11 01:40:58 +0800

    new pic, ppm and mem

--- final_snake/lab10_for_student/Lab10/lab10/lab10.runs/impl_1/rundef.js
+++ final_snake/lab10_for_student/Lab10/lab10/lab10.runs/impl_1/rundef.js
@@ -24,7 +24,7 @@ eval( EAInclude(ISEJScriptLib) );
 
 
 // pre-commands:
-ISETouchFile( "init_design", "begin" );
+ISETouchFile( "write_bitstream", "begin" );
 ISEStep( "vivado",
          "-log lab10.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source lab10.tcl -notrace" );
 
--- final_snake/lab10_for_student/Lab10/lab10/lab10.runs/impl_1/runme.sh
+++ final_snake/lab10_for_student/Lab10/lab10/lab10.runs/impl_1/runme.sh
@@ -42,7 +42,7 @@ EAStep()
 }
 
 # pre-commands:
-/bin/touch .init_design.begin.rst
+/bin/touch .write_bitstream.begin.rst
 EAStep vivado -log lab10.vdi -applog -m64 -product Vivado -messageDb vivado.pb -mode batch -source lab10.tcl -notrace
 
 
