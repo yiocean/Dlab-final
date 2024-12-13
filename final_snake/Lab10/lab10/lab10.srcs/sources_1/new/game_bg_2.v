435bb1f36ec82cdedff7ea8412fcc7a13a861522
unknown <qomolanma0206@gmail.com>
2024-12-13 13:25:21 +0800

    playable snake without scoring display and food display

---
+++ final_snake/Lab10/lab10/lab10.srcs/sources_1/new/game_bg_2.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/12/13 11:36:42
+// Design Name: 
+// Module Name: game_bg_2
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module game_bg_2(
+
+    );
+endmodule
